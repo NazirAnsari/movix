979ad75cb871c4a012fcb481841342b18efa866d
Nazir Ansari <naziransari989@gmail.com>
2026-04-05 13:24:51 +0530

    feat: update css

--- movix/src/pages/404/PageNotFound.jsx
+++ movix/src/pages/404/PageNotFound.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import './style.scss'
+import React from "react";
+
+import "./style.scss";
+
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 
 const PageNotFound = () => {
     return (
-        <div>
-            Page not found
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
         </div>
-    )
-}
-import './style.scss'
+    );
+};
 
-export default PageNotFound
+export default PageNotFound;
